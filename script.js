ccf6e801fed392fd436252e4e50d2c5c8b3810ee
Lorenzo De Simone <lorenzo.desimone2007@gmail.com>
2025-05-06 19:42:00 +0200

    script e form

---
+++ script.js
@@ -0,0 +1,16 @@
+
+/* Error filling out the form */
+
+const form = document.querySelector("form");
+const email =  document.getElementById("email");
+const password =  document.getElementById("password");
+const errorElement = document.getElementById("error-message");
+
+form.addEventListener("submit", function(e) { e.preventDefault(); 
+    if (!email.ariaValueMax.trim() || !password.value.trim()) {
+        errorElement.textContent = "Please fill out all fields.";
+        errorElement.style.display = "block";
+    } else {
+        errorElement.style.display = "none";
+    }
+});
