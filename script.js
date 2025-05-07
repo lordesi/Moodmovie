630894e25c33d069550c55b99d4088433611df59
Lorenzo De Simone <lorenzo.desimone2007@gmail.com>
2025-05-07 22:49:58 +0200

    search bar top animation

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const password =  document.getElementById("password");
 const errorElement = document.getElementById("error-message");
 
 form.addEventListener("submit", function(e) { e.preventDefault(); 
-    if (!email.ariaValueMax.trim() || !password.value.trim()) {
+    if (!emaSil.value.trim() || !password.value.trim()) {
         errorElement.textContent = "Please fill out all fields.";
         errorElement.style.display = "block";
     } else {
